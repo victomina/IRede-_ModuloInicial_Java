c457a249b19403b642002eb66848b04a21881f88
victomina <victomina@protonmail.com>
2026-06-17 21:35:55 -0300

    Adicionando livro e projetos de Java

---
+++ NetBeans_projects/VictominaHello.java
@@ -0,0 +1,12 @@
+package com.mycompany.victominahello;
+
+/**
+ *
+ * @author Usuário
+ */
+public class VictominaHello {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World! Victomina");
+    }
+}
